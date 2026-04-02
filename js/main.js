c55da6f4588879d19750a01421ecd644ae21b4e7
Erika Yokota <rin0000ki@gmail.com>
2026-04-02 16:33:25 +0900

    newsページ完了

---
+++ js/main.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('[role="tab"]');
+    const tabList = document.querySelector('[role="tablist"]');
+
+    if (!tabList) return;
+
+    tabs.forEach(tab => {
+        tab.addEventListener('click', changeTabs);
+    });
+
+    let tabFocus = 0;
+    tabList.addEventListener("keydown", e => {
+        if (e.keyCode === 37 || e.keyCode === 39) {
+            tabs[tabFocus].setAttribute("tabindex", -1);
+            if (e.keyCode === 37) {
+                tabFocus--;
+                if (tabFocus < 0) tabFocus = tabs.length - 1;
+            } else if (e.keyCode === 39) {
+                tabFocus++;
+                if (tabFocus >= tabs.length) tabFocus = 0;
+            }
+            tabs[tabFocus].setAttribute("tabindex", 0);
+            tabs[tabFocus].click();
+            tabs[tabFocus].focus();
+        }
+    });
+});
+
+function changeTabs(e) {
+    const target = e.currentTarget;
+    const controlsId = target.getAttribute("aria-controls");
+    const panels = document.querySelectorAll('.l-tab');
+
+    if (!controlsId) return;
+
+
+    // 選択されているタブのみ下線を太くする
+    panels.forEach(p => {
+        p.classList.toggle('c-tab_border');
+    })
+
+
+    const parent = target.closest('[role="tablist"]');
+    // section.l-news 内からパネルを探す
+    const container = parent.closest('.l-news');
+
+    // 1. すべてのタブの aria-selected を更新
+    parent.querySelectorAll('[role="tab"]').forEach(tab => {
+        tab.setAttribute("aria-selected", tab === target ? "true" : "false");
+    });
+
+    // 2. すべてのパネルを隠す
+    container.querySelectorAll('[role="tabpanel"]').forEach(panel => {
+        panel.classList.add('is-hidden');
+    });
+
+    // 3. 対象のパネルを表示
+    const targetPanel = document.getElementById(controlsId);
+    if (targetPanel) {
+        targetPanel.classList.remove('is-hidden');
+    }
+
+
+
+}
